ac6330d4921bcd1e7cea6b02f752d0d1c3c86f48
Jahaziel <jahaziel.vaz@gmail.com>
2019-10-17 21:08:16 -0600

    Counter now works

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 let btn = document.getElementById('btn');
+let animalInfo = document.getElementById('animal-info');
+let counter = 1;
 
 btn.addEventListener('click', () => {
     let myRequest = new XMLHttpRequest();
 
-    myRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
+    myRequest.open('GET', `https://learnwebcode.github.io/json-example/animals-${counter}.json`);
 
     myRequest.onload = () => {
       let animals = JSON.parse(myRequest.responseText);
@@ -11,8 +13,15 @@ btn.addEventListener('click', () => {
     }
 
     myRequest.send();
+
+    counter++
 })
 
-let renderHTML = (animals) => {
-   console.log(animals)
+let renderHTML = (data) => {
+  let htmlString = ''
+  for(let i = 0; i < data.length; i++){
+    htmlString += `<p> ${data[i].name} is a ${data[i].species}. </p> `;
+  }
+
+   animalInfo.insertAdjacentHTML('beforeend', htmlString)
 }
